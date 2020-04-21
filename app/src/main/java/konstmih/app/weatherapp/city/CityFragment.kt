2cedd6d5be81f9cec9c87f7ab1b183ecc053081a
Konstantin Mihayloff <mihayloff.konst@gmail.com>
2020-04-21 16:26:57 +0200

    Creation of city dialog and city fragment with option menu

--- app/src/main/java/konstmih/app/weatherapp/city/CityFragment.kt
+++ app/src/main/java/konstmih/app/weatherapp/city/CityFragment.kt
@@ -1,19 +1,84 @@
 package konstmih.app.weatherapp.city
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import konstmih.app.weatherapp.App
+import konstmih.app.weatherapp.Database
 import konstmih.app.weatherapp.R
 
 class CityFragment : Fragment() {
+
+    private lateinit var cities: MutableList<City>
+    private lateinit var database: Database
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Database
+        database = App.database
+
+        // List of cities
+        cities = mutableListOf()
+
+        // Notify fragment that it has an option menu
+        // So it will use onCreateOptionsMenu
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflating XML layout file
         val view = inflater?.inflate(R.layout.fragment_city, container, false)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // Inflating XML layout file
+        inflater.inflate(R.menu.fragment_city, menu)
+    }
+
+    // If item from options menu are selected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.action_create_city -> {
+                // Run CreateCity Dialog
+                showCreateCityDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // Creating dialog fragment
+    private fun showCreateCityDialog() {
+        val createCityFragment = CreateCityDialogFragment()
+
+        // Listener interface
+        createCityFragment.listener = object: CreateCityDialogFragment.CreateCityDialogListener{
+            // Overriding interface listener functions
+            override fun onDialogPositiveClick(cityName: String) {
+                saveCity(City(cityName))
+
+            }
+
+            override fun onDialogNegativeClick() { }
+        }
+
+        // Display the dialog fragment
+        fragmentManager?.let { createCityFragment.show(it, "CreateCityDialogFragment") }
+    }
+
+    private fun saveCity(city: City)
+    {
+        if(database.createCity(city)) { // Adding city in database
+            cities.add(city) // if success than it can be added to the mutableList cities
+        } else {
+            Toast.makeText(context, "Could not create city", Toast.LENGTH_SHORT).show() // Error Toast
+        }
     }
 }
